6bdf82cab01b5efb96ad7f4138bd0ce2eafe3901
ThheKNG <kkarundu8@gmail.com>
2023-05-08 22:06:16 +0100

    remove services

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { WeatherService } from '../services/weather.service';
-import { log } from 'console';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage  {
 
-  current_weather:any = [];
+  constructor() {}
 
-  constructor(private weather:WeatherService) {}
-
-  ngOnInit() {
-      this.weather.get_current_weather().subscribe(data => {
-        this.current_weather = data.current_weather;
-      })
-  }
 
 }

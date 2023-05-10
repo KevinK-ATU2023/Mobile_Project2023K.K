9edb03f10048fb192835c3ed5abe54758f1cab1b
ThheKNG <kkarundu8@gmail.com>
2023-05-10 11:46:10 +0100

    Finish HomePage

--- src/app/Services/entertainment.service.ts
+++ src/app/Services/entertainment.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, SimpleChanges } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -11,8 +11,12 @@ export class EntertainmentService {
 
   constructor(private http:HttpClient) { }
 
-  search_movies(query: string):Observable<any> {
+  search_movies(query: any):Observable<any> {
     return this.http.get(`${environment.base_url}/search/movie?api_key=${environment.api_key}&language=en-US&query=${query}&page=1&include_adult=false`);
+  }
+
+  search_tv(query: any):Observable<any> {
+    return this.http.get(`${environment.base_url}/search/tv?api_key=${environment.api_key}&language=en-US&query=${query}&page=1&include_adult=false`);
   }
 
   get_popular_movies():Observable<any> {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit,} from '@angular/core';
 import { EntertainmentService } from '../Services/entertainment.service';
 import { environment } from 'src/environments/environment';
 import { log } from 'console';
@@ -10,19 +10,75 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 
-export class HomePage implements OnInit  {
+export class HomePage implements OnInit {
 
   constructor(private navCtrl: NavController,private entertainService: EntertainmentService) {}
 
   movie: any = [];
-  movie_poster = environment.posters;
+  tv: any = [];
+
+  hidden_movie: any = [];
+  hidden_tv: any = [];
+
+  input_placeholder: string = "";
+
+  movie_search_toggle: boolean = true;
+  tv_search_toggle: boolean = false;
+
+  poster = environment.posters;
   
-  entertain_input: any;
+  user_search: any = "";
 
   ngOnInit(): void {
-      this.entertainService.search_movies(this.entertain_input).subscribe(data => {
+    if (this.movie_search_toggle) {
+      this.input_placeholder = "Search Movie";
+    }
+    if (this.tv_search_toggle) {
+      this.input_placeholder = "Search TV";
+    }
+  }
+
+  toggle_movie_tv() {
+    this.movie_search_toggle = !this.movie_search_toggle;
+    this.tv_search_toggle = !this.tv_search_toggle;
+
+    if (this.movie_search_toggle) {
+      console.log("movie search activated");
+    }
+    else {
+      console.log("movie search deactivated");
+    }
+    if (this.tv_search_toggle) {
+      console.log("tv search activated");
+    }
+    else {
+      console.log("tv search deactivated");
+    }
+
+    if (this.movie_search_toggle) {
+      this.input_placeholder = "Search Movie";
+    }
+    if (this.tv_search_toggle) {
+      this.input_placeholder = "Search TV";
+    }
+  }
+
+  search_button_click() {
+    if (this.movie_search_toggle) {
+      this.entertainService.search_movies(this.user_search).subscribe(data => {
         this.movie = data.results;
+        this.hidden_movie = false;
+        this.hidden_tv = true;
       });
+    }
+    if (this.tv_search_toggle) {
+      this.entertainService.search_tv(this.user_search).subscribe(data => {
+        this.tv = data.results;
+        this.hidden_movie = true;
+        this.hidden_tv = false;
+        console.log(this.tv);
+      });
+    }
   }
 
   open_popular_movies(){
